88f94f3074188d3a140ac851e6f0d1adc328b998
Qun12est <georgekperdikas@gmail.com>
2026-06-24 11:12:11 +0300

    Add normalization + unit tests

--- unit-tests/normalizeSeverity.spec.js
+++ unit-tests/normalizeSeverity.spec.js
@@ -1,5 +1,6 @@
 const {test, expect} = require('@playwright/test');
 const {normalizeOneSeverity} = require('../src/normalize-severity');
+const {normalizeSeverities} = require('../src/normalize-severity');
 
 test('Normalize mixed Upper & Lower case wording', () => {
   const result = normalizeOneSeverity('CRIticAl');
@@ -16,7 +17,52 @@ test('Normalize only Lower case wording', () => {
   expect(result).toBe('Minor');
 });
 
-test('Try to normalize other wording returns undefined', () => {
-  const result = normalizeOneSeverity('red code');
-  expect(result).toBe('red code');
+test('Try to normalize other wording returns original value', () => {
+  const result = normalizeOneSeverity('Red code');
+  expect(result).toBe('Red code');
+});
+
+test('Normalise all-capitals valid severity', () => {
+  const result = normalizeSeverities([
+    {severity: 'MAJOR', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+  expect(result).toEqual([
+    {severity: 'Major', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+});
+
+test('Preserves unknown severity', () => {
+  const result = normalizeSeverities([
+    {severity: 'MEdium', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+  expect(result).toEqual([
+    {severity: 'MEdium', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+});
+
+test('Normalise 2 valid severities', () => {
+  const result = normalizeSeverities([
+    {severity: 'CRItical', title: 'Missing button', description: 'Save button is missing on bottom-right corner'},
+    {severity: 'MAJOR', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+  expect(result).toEqual([
+    {severity: 'Critical', title: 'Missing button', description: 'Save button is missing on bottom-right corner'},
+    {severity: 'Major', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+});
+
+test('Normalise 1 valid severities and preserve 1 unknown', () => {
+  const result = normalizeSeverities([
+    {severity: 'CRItical', title: 'Missing button', description: 'Save button is missing on bottom-right corner'},
+    {severity: 'MEDIUM', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+  expect(result).toEqual([
+    {severity: 'Critical', title: 'Missing button', description: 'Save button is missing on bottom-right corner'},
+    {severity: 'MEDIUM', title: 'Missing button', description: 'Save button is missing on bottom-right corner'}
+  ]);
+});
+
+test('Preserve empty array', () => {
+  const result = normalizeSeverities([]);
+  expect(result).toEqual([]);
 });
