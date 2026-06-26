63789e9f3404f4f6e0e36f5c589fa530c68ea4d0
Qun12est <georgekperdikas@gmail.com>
2026-06-26 15:22:35 +0300

    Parameterize and export runTool

--- src/run-tool.js
+++ src/run-tool.js
@@ -7,10 +7,7 @@ const baselinePath = "test-data/designs/login-form-baseline.png";
 // const currentPath = "test-data/screenshots/dummy-current.png";
 let riskResult ;
 
-async function runTool() {
-  const url = "http://localhost:3000/page1";
-  const locator = "#login-form";
-  const savePath = "test-data/screenshots/";
+async function runTool(url, locator, savePath) {
   const currentPath = await takeScreenshot(url, locator, savePath);
   const result = await compareImages(baselinePath, currentPath);
   if (result.escalatedToAi === true) {
@@ -23,4 +20,4 @@ async function runTool() {
 const finalResult = { risk: riskResult, analysis: result.analysis, issues: result.issues, differencePercentage: result.differencePercentage };
 console.log(finalResult);
 }
-runTool();
+module.exports = {runTool};
